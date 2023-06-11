6a363aa0fb2039cf05be104a60574fa9ec3a64e4
nelson <htop000@yandex.ru>
2023-06-12 00:46:43 +0300

    Первый рабочий вариант готов

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from datetime import datetime, date, timedelta
 from pydantic import BaseModel, constr, conint, confloat
-from fastapi import FastAPI, Request, status
+from fastapi import FastAPI, status
 from fastapi.exceptions import RequestValidationError
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
@@ -22,8 +22,7 @@ def last_day_of_month(day: date) -> date:
 
 
 @app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request,
-                                       exc: RequestValidationError):
+async def validation_exception_handler(exc: RequestValidationError):
     return JSONResponse(
         status_code=status.HTTP_400_BAD_REQUEST,
         content=jsonable_encoder({"error": exc.errors()[0]['msg']})
