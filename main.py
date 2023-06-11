e76634c65dc1f3d0f984e1eb837400e2231fdcc7
nelson <htop000@yandex.ru>
2023-06-11 09:57:49 +0300

    Добавлена валидация данных

--- main.py
+++ main.py
@@ -1,48 +1,50 @@
-from fastapi import FastAPI
 from datetime import datetime, date, timedelta
-
-app = FastAPI()
+from pydantic import BaseModel, constr, conint, confloat
+from fastapi import FastAPI
 
 
 def last_day_of_month(day: date) -> date:
-    '''
+    """
     Принимает на вход дату и возвращает последний
     день месяца из этой даты
     :param day:
     :return: возвращает последний
     день месяца из этой даты
-    '''
+    """
     next_month = day.replace(day=28) + timedelta(days=4)  # this will never fail
     return next_month - timedelta(days=next_month.day)
 
 
-@app.get("/")
-async def read_item(start_date: str = None,
-                    periods: int = None,
-                    amount: int = None,
-                    rate: float = None
-                    ):
-    # TODO Валдация
+class InputData(BaseModel):
+    date: constr(regex=r'\d{2}.\d{2}.\d{4}') = '31.01.2021'
+    periods: conint(ge=1, le=60) = 3
+    amount: conint(ge=10000, le=3000000) = 10000
+    rate: confloat(ge=1.0, le=8.0) = 6
 
-    print(start_date, periods, amount, rate)
 
+app = FastAPI()
+
+
+@app.post("/")
+def read_item(input_data: InputData):
     output = {}
-    start_date = datetime.strptime(start_date, '%d.%m.%Y')
 
-    date_not_str = last_day_of_month(start_date)
+    request_date = datetime.strptime(input_data.date, '%d.%m.%Y')
 
-    while periods > 0:
-        date_str = date_not_str.strftime('%d.%m.%Y')
-        summa = amount * (1 + rate / 12 / 100)
-        output[date_str] = round(summa, 2)
+    last_day_of_period = last_day_of_month(request_date)
 
-        # Обновляем дату последнего дня следующего месяа
-        next_day = date_not_str + timedelta(days=1)
-        date_not_str = last_day_of_month(next_day)
+    # В цикле проходим по всем периодам
+    while input_data.periods > 0:
+        summa = input_data.amount * (1 + input_data.rate / 12 / 100)
+        output[last_day_of_period.strftime('%d.%m.%Y')] = round(summa, 2)
+
+        # Обновляем дату последнего дня следующего месяца
+        next_day = last_day_of_period + timedelta(days=1)
+        last_day_of_period = last_day_of_month(next_day)
 
         # Обновляем счетчик периодов
-        periods -= 1
+        input_data.periods -= 1
         # Обновляем сумму
-        amount = summa
+        input_data.amount = summa
 
-    return output, 200
+    return {'Status': 'OK', 'Response': output}
