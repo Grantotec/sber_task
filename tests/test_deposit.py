226c31106840c2c1470cea00fbdb37ad12816c59
nelson <htop000@yandex.ru>
2023-06-13 13:14:25 +0300

    Добавлен Dockerfile

--- tests/test_deposit.py
+++ tests/test_deposit.py
@@ -4,13 +4,9 @@ from datetime import date
 
 
 class Test(TestCase):
-
     def test_last_day_of_month(self):
         self.assertEqual(last_day_of_month(date(2021, 1, 31)),
                          date(2021, 1, 31))
-
-    def test_deposit(self):
-        pass
 
 
 if __name__ == '__main__':
